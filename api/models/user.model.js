46758e9bb370bcdd6cce828c105d8c39607d42e0
SUSHANTH R HEGDE <sushanthhebri6@gmail.com>
2024-07-06 11:08:47 +0530

    added google Auth functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,24 +1,30 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-
-    username :{
-        type : String,
-        required : true,
-        unique : true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email :{
-        type : String,
-        required : true,
-        unique : true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password :{
-        type : String,
-        required : true,
-    }, 
-},
-{ timestamps : true}
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        'https://p16-va.lemon8cdn.com/tos-alisg-v-a3e477-sg/oABbQEOID5njzCOHgEfME6AAAF9eAlB9r9Xt6v~tplv-tej9nj120t-origin.webp',
+    },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
+
 export default User;
