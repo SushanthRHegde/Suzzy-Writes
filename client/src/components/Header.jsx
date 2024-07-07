e2aaf8d6fe001fc44d0c2b78465a051219301c95
SUSHANTH R HEGDE <sushanthhebri6@gmail.com>
2024-07-07 21:54:55 +0530

    added home page and search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react';
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleNavbar = () => {
@@ -32,6 +45,14 @@ export default function Header() {
       console.log(error.message);
     }
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <Navbar className='border-b-2'>
       <div className='flex items-center justify-between w-full'>
@@ -45,11 +66,13 @@ export default function Header() {
             </span>
             WritingZ
           </Link>
-          <form className='hidden lg:flex md:flex'>
+          <form className='hidden lg:flex md:flex' onSubmit={handleSubmit}>
             <TextInput
               type='text'
               placeholder='Search...'
               rightIcon={AiOutlineSearch}
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
           </form>
           <Button className='w-12 h-10 lg:hidden md:hidden' color='gray' pill>
