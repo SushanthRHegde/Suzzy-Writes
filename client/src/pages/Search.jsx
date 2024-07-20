a8af1293658dfd6d1cdf04211f8287ee38c37153
SUSHANTH R HEGDE <sushanthhebri6@gmail.com>
2024-07-20 23:15:33 +0530

    modified search section

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -131,9 +131,9 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='Life'>Life</option>
+              <option value='Tech'>Tech</option>
+              <option value='Finance'>Finance</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
